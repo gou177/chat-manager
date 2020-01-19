21d80caf9653579b7f55bcc60705aeddc08e077a
In Devolopment <bdmitry450@gmail.com>
2020-01-19 20:30:55 +0300

    Добавил функцию сбора

--- commands/online_check.py
+++ commands/online_check.py
@@ -2,11 +2,11 @@
 from common.Store import Stoaring
 from utils import *
 
-plugin = EPlugin(theme="aaaaaa")
+plugin = EPlugin(theme="ConvUtils")
 
 
 @plugin.on_command(["online", "онлайн"])
-def check(args, store: Stoaring):
+def check(args: list, store: Stoaring):
     msg = "\nСейчас онлайн:"
     online = []
     members = store.vk.messages.getConversationMessages(peer_id=store.peer_id)['profiles']
@@ -19,3 +19,17 @@ def check(args, store: Stoaring):
 
     msg += "\n".join(online)
     return store.send(msg)
+
+@plugin.on_command(["summon", "сбор"])
+def summon(args: list, store: Stoaring):
+    online = False
+    result = "\nОбщий сбор!\n"
+    if len(args) == 1:
+        if "онлайн" in args or "online" in args:
+            online = True
+    members = store.vk.messages.getConversationMessages(peer_id=store.peer_id)['profiles']
+    for mem in members:
+        if mem['online'] or not online:
+            if 'screen_name' not in mem.keys: return # Если человек DELETED
+            result += '['+mem['screen_name']+'|'+mem['first_name']+'] '
+    store.send(result)
